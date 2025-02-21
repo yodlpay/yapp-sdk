92fc3552df7cd9be6e06cc22d06c6341dc3cc553
mihkel <joll.mihkeel@gmail.com>
2025-02-21 10:55:08 +0200

    chore: configure Jest code coverage reporting

--- jest.config.js
+++ jest.config.js
@@ -7,4 +7,13 @@ module.exports = {
     '^.+\\.ts$': 'ts-jest',
   },
   testMatch: ['<rootDir>/src/**/*.test.ts'],
+  collectCoverage: true,
+  coverageDirectory: 'coverage',
+  coverageReporters: ['text', 'lcov', 'html'],
+  collectCoverageFrom: [
+    'src/**/*.{ts,tsx}',
+    '!src/**/*.test.{ts,tsx}',
+    '!src/**/*.d.ts',
+    '!src/constants/keys.ts'
+  ]
 };
